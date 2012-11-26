ff25f5c234a772e879b2a86436d5e7fc5fcd715a
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-11-26 09:03:01 +0000

    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@145 0e92fcd8-05cc-4690-8748-b30b12f507e5

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Irony;
+using Irony.Ast;
+using Irony.Parsing;
+
+using ETUS.DomainModel;
+using ETUS.Grammar;
+
+namespace ConsoleApplication1
+{
+    class Person
+    {
+        public int n = 5;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Func<Person> foo = () => person;
+            Person p1 = foo();
+            person.n = 8;
+            Person p2 = foo();
+
+            var grammar = new UDLGrammar();
+            var parser = new Parser(grammar);
+            ParseTree parseTree = parser.Parse("declare namespace ETUS");
+        }
+    }
+}
