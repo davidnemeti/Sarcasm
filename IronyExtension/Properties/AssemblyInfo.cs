18260693a9c0fa8b46aa926a7d504f330d9a4cbe
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-12-04 15:36:58 +0000

    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@172 0e92fcd8-05cc-4690-8748-b30b12f507e5

--- IronyExtension/Properties/AssemblyInfo.cs
+++ IronyExtension/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IronyExtension")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extensions for Irony: property, field and type binding for terminals and non-terminals; automatic unparser based on bound grammar")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IronyExtension")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2012, Dávid Németi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
