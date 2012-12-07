7c1b3bd1e1805a7585c8cbf7f231d70141309fdd
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-12-07 11:59:18 +0000

    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@201 0e92fcd8-05cc-4690-8748-b30b12f507e5

--- IronyExtension/AstBinders/TypeForCollection.cs
+++ IronyExtension/AstBinders/TypeForCollection.cs
@@ -15,55 +15,60 @@ namespace Irony.AstBinders
 {
     public class TypeForCollection : TypeForNonTerminal
     {
-        // NOTE: this type has to be in syncron with the type constraint in TypeForCollection<TCollectionStaticType> and TypeForCollection<TCollectionStaticType, TElementType>
-        private static readonly Type collectionBaseGenericTypeDefinition = typeof(ICollection<>);
+        //private static readonly Type iCollectionGenericTypeDefinition = typeof(ICollection<>);
+        //private static readonly Type iCollectionType = typeof(ICollection<>);
 
-        protected Type collectionDynamicType { get { return base.type; } }
+        protected Type collectionType { get { return base.type; } }
         private readonly Type elementType;
+        private readonly MethodInfo addMethodInfo;
 
         [Obsolete("Use collectionDynamicType instead", error: true)]
         protected new Type type { get { return base.type; } }
 
-        protected TypeForCollection(Type collectionDynamicType, string errorAlias)
-            : base(collectionDynamicType, errorAlias)
+        protected const BindingFlags bindingAttrInstanceAll = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        protected TypeForCollection(Type collectionType, Type elementType, string errorAlias, bool runtimeCheck)
+            : base(collectionType, errorAlias)
         {
-            if (collectionDynamicType.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, binder: null, types: Type.EmptyTypes, modifiers: null) == null)
-                throw new ArgumentException("Type has no default constructor (neither public nor nonpublic)", "type");
+            if (runtimeCheck && collectionType.GetConstructor(bindingAttrInstanceAll, Type.DefaultBinder, types: Type.EmptyTypes, modifiers: null) == null)
+                throw new ArgumentException("Collection type has no default constructor (neither public nor nonpublic)", "type");
 
-            Type collectionDynamicBaseGenericDefinition = collectionDynamicType.GetInterfaces().FirstOrDefault(interfaceType => interfaceType.GetGenericTypeDefinition() == collectionBaseGenericTypeDefinition);
+            if (runtimeCheck && elementType == null && collectionType.IsGenericType)
+            {   // we try to guess the elementType
+                Type iCollectionGenericType = collectionType.GetInterfaces().FirstOrDefault(interfaceType => interfaceType.GetGenericTypeDefinition() == typeof(ICollection<>));
+                if (iCollectionGenericType != null)
+                    elementType = iCollectionGenericType.GenericTypeArguments[0];
+            }
+            this.elementType = elementType ?? typeof(object);
 
-            if (collectionDynamicBaseGenericDefinition == null)
-                throw new ArgumentException(string.Format("Type does not implement {0}", collectionBaseGenericTypeDefinition.FullName), "type");
-
-            this.elementType = collectionDynamicBaseGenericDefinition.GetGenericArguments()[0];
+            if (runtimeCheck)
+            {
+                addMethodInfo = collectionType.GetMethod("Add", bindingAttrInstanceAll, Type.DefaultBinder, new[]{elementType}, modifiers: null);
+                if (addMethodInfo == null)
+                    throw new ArgumentException("Collection type has proper 'Add' method (neither public nor nonpublic)", "collectionType");
+            }
         }
 
-        public static TypeForCollection<TCollectionStaticType> Of<TCollectionStaticType>(string errorAlias = null)
-            where TCollectionStaticType : new()
+        public static TypeForCollection<TCollectionType> Of<TCollectionType>(string errorAlias = null)
+            where TCollectionType : System.Collections.IList, new()
         {
-            return new TypeForCollection<TCollectionStaticType>(typeof(TCollectionStaticType), errorAlias);
+            return new TypeForCollection<TCollectionType>(errorAlias);
         }
 
-        public static TypeForCollection<TCollectionStaticType> Of<TCollectionStaticType>(Type collectionDynamicType, string errorAlias = null)
+        public static TypeForCollection<TCollectionType, TElementType> Of<TCollectionType, TElementType>(string errorAlias = null)
+            where TCollectionType : ICollection<TElementType>, new()
         {
-            return new TypeForCollection<TCollectionStaticType>(collectionDynamicType, errorAlias);
+            return new TypeForCollection<TCollectionType, TElementType>(errorAlias);
         }
 
-        public static TypeForCollection<TCollectionStaticType> Of<TCollectionStaticType, TElementType>(string errorAlias = null)
-            where TCollectionStaticType : ICollection<TElementType>, new()
+        public static TypeForCollection Of(Type collectionType, string errorAlias = null)
         {
-            return new TypeForCollection<TCollectionStaticType, TElementType>(typeof(TCollectionStaticType), errorAlias);
+            return new TypeForCollection(collectionType, elementType: null, errorAlias: errorAlias, runtimeCheck: true);
         }
 
-        public static TypeForCollection<TCollectionStaticType> Of<TCollectionStaticType, TElementType>(Type collectionDynamicType, string errorAlias = null)
-            where TCollectionStaticType : ICollection<TElementType>
+        public static TypeForCollection Of(Type collectionType, Type elementType, string errorAlias = null)
         {
-            return new TypeForCollection<TCollectionStaticType, TElementType>(collectionDynamicType, errorAlias);
-        }
-
-        public static TypeForCollection Of(Type collectionDynamicType, string errorAlias = null)
-        {
-            return new TypeForCollection(collectionDynamicType, errorAlias);
+            return new TypeForCollection(collectionType, elementType, errorAlias, runtimeCheck: true);
         }
 
         public override BnfExpression Rule
@@ -73,13 +78,17 @@ namespace Irony.AstBinders
             {
                 AstConfig.NodeCreator = (context, parseTreeNode) =>
                 {
-                    dynamic collection = Activator.CreateInstance(collectionDynamicType, nonPublic: true);
+                    /*
+                     * NOTE: ICollection does not have an Add method (only ICollection<> has), IList might be overkill to enforce an Add method,
+                     * so we just working here with an object type and require that the collection has an Add method during runtime (we are using reflection here anyway)
+                     * */
+                    object collection = Activator.CreateInstance(collectionType, nonPublic: true);
 
                     foreach (var parseTreeChild in parseTreeNode.ChildNodes)
                     {
                         if (parseTreeChild.AstNode.GetType() == elementType)
                         {
-                            collection.Add(parseTreeChild.AstNode);
+                            addMethodInfo.Invoke(obj: collection, parameters: new[]{parseTreeChild.AstNode});
                         }
                         else if (!parseTreeChild.Term.Flags.IsSet(TermFlags.NoAstNode))
                         {
@@ -91,33 +100,113 @@ namespace Irony.AstBinders
                     parseTreeNode.AstNode = collection;
                 };
 
+                //CreateAndFillCollection<object, object>(
+                //    () => Activator.CreateInstance(collectionType, nonPublic: true),
+                //    (collection, element) => addMethodInfo.Invoke(obj: collection, parameters: new[]{element}),
+                //    elementDynamicType: elementType);
+
+                base.Rule = value;
+            }
+        }
+
+        //protected void CreateAndFillCollection<TCollectionStaticType, TElementStaticType>(Func<TCollectionStaticType> createCollection, Action<TCollectionStaticType, TElementStaticType> addElementToCollection)
+        //{
+        //    CreateAndFillCollection(createCollection, addElementToCollection, elementDynamicType: typeof(TElementStaticType));
+        //}
+
+        //protected void CreateAndFillCollection<TCollectionStaticType, TElementStaticType>(Func<TCollectionStaticType> createCollection, Action<TCollectionStaticType, TElementStaticType> addElementToCollection, Type elementDynamicType)
+        //{
+        //}
+    }
+
+    public class TypeForCollection<TCollectionType> : TypeForCollection, IBnfTerm<TCollectionType>
+        where TCollectionType : System.Collections.IList, new()
+    {
+        internal TypeForCollection(string errorAlias)
+            : base(typeof(TCollectionType), typeof(object), errorAlias: errorAlias, runtimeCheck: false)
+        {
+        }
+
+        protected TypeForCollection(Type collectionType, Type elementType, string errorAlias)
+            : base(collectionType, elementType, errorAlias, runtimeCheck: false)
+        {
+        }
+
+        BnfTerm IBnfTerm<TCollectionType>.AsTypeless()
+        {
+            return this;
+        }
+
+        public override BnfExpression Rule
+        {
+            get { return base.Rule; }
+            set
+            {
+                AstConfig.NodeCreator = (context, parseTreeNode) =>
+                {
+                    TCollectionType collection = new TCollectionType();
+
+                    foreach (var parseTreeChild in parseTreeNode.ChildNodes)
+                    {
+                        if (parseTreeChild.AstNode.GetType() == typeof(object))
+                        {
+                            collection.Add(parseTreeChild.AstNode);
+                        }
+                        else if (!parseTreeChild.Term.Flags.IsSet(TermFlags.NoAstNode))
+                        {
+                            context.AddMessage(ErrorLevel.Error, parseTreeChild.Token.Location, "Term '{0}' should be type of '{1}' but found '{2}' instead",
+                                parseTreeChild.Term, typeof(object).FullName, parseTreeChild.AstNode.GetType().FullName);
+                        }
+                    }
+
+                    parseTreeNode.AstNode = collection;
+                };
+
                 base.Rule = value;
             }
         }
     }
 
-    public class TypeForCollection<TCollectionStaticType> : TypeForCollection, IBnfTerm<TCollectionStaticType>
-    //        where TCollectionStaticType : ICollection<>   // unfortunately C# does not allow to write this, so no compile time check can be done here (runtime check is in done in TypeForCollection)
-        // NOTE: this type constraint has to be in syncron with the type stored in the value of TypeForCollection.collectionBaseGenericTypeDefinition
+    public class TypeForCollection<TCollectionType, TElementType> : TypeForCollection, IBnfTerm<TCollectionType>
+        where TCollectionType : ICollection<TElementType>, new()
     {
-        internal TypeForCollection(Type collectionDynamicType, string errorAlias)
-            : base(collectionDynamicType, errorAlias)
+        internal TypeForCollection(string errorAlias)
+            : base(typeof(TCollectionType), typeof(TElementType), errorAlias, runtimeCheck: false)
         {
         }
 
-        BnfTerm IBnfTerm<TCollectionStaticType>.AsTypeless()
+        BnfTerm IBnfTerm<TCollectionType>.AsTypeless()
         {
             return this;
         }
-    }
 
-    public class TypeForCollection<TCollectionStaticType, TElementType> : TypeForCollection<TCollectionStaticType>
-        where TCollectionStaticType : ICollection<TElementType>
-        // NOTE: this type constraint has to be in syncron with the type stored in the value of TypeForCollection.collectionBaseGenericTypeDefinition
-    {
-        internal TypeForCollection(Type collectionDynamicType, string errorAlias)
-            : base(collectionDynamicType, errorAlias)
+        public override BnfExpression Rule
         {
+            get { return base.Rule; }
+            set
+            {
+                AstConfig.NodeCreator = (context, parseTreeNode) =>
+                {
+                    TCollectionType collection = new TCollectionType();
+
+                    foreach (var parseTreeChild in parseTreeNode.ChildNodes)
+                    {
+                        if (parseTreeChild.AstNode.GetType() == typeof(TElementType))
+                        {
+                            collection.Add((TElementType)parseTreeChild.AstNode);
+                        }
+                        else if (!parseTreeChild.Term.Flags.IsSet(TermFlags.NoAstNode))
+                        {
+                            context.AddMessage(ErrorLevel.Error, parseTreeChild.Token.Location, "Term '{0}' should be type of '{1}' but found '{2}' instead",
+                                parseTreeChild.Term, typeof(TElementType).FullName, parseTreeChild.AstNode.GetType().FullName);
+                        }
+                    }
+
+                    parseTreeNode.AstNode = collection;
+                };
+
+                base.Rule = value;
+            }
         }
     }
 }
