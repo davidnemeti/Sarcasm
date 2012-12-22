b09f4bf30bbc337576c17af76d554176cec50f5d
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2012-12-22 18:04:34 +0000

    MemberwiseCopy
    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@337 0e92fcd8-05cc-4690-8748-b30b12f507e5

--- Irony.ITG/BnfiTerms/BnfiTermType.cs
+++ Irony.ITG/BnfiTerms/BnfiTermType.cs
@@ -32,15 +32,14 @@ namespace Irony.ITG
                 {
                     object objValue = Activator.CreateInstance(type, nonPublic: true);
 
-                    //foreach (var parseTreeChild in parseTreeNode.ChildNodes.Where(childNode => childNode.Tag is BnfiTermType))
-                    //{
-                    //    BnfiTermType sourceBnfiTermType = (BnfiTermType)parseTreeChild.Tag;
-                    //    object sourceObjValue = GrammarHelper.AstNodeToValue(parseTreeChild.AstNode);
+                    var childValues = parseTreeNode.ChildNodes.Select(childNode => GrammarHelper.AstNodeToValue(childNode.AstNode)).Where(childNode => childNode != null);
 
+                    // 1. memberwise copy for ast values
+                    foreach (var childValue in childValues.Where(childValue => objValue.GetType().IsAssignableFrom(childValue.GetType())))
+                        MemberwiseCopyExceptNullValues(objValue, childValue);
 
-                    //}
-
-                    foreach (var memberValue in parseTreeNode.ChildNodes.Select(childNode => GrammarHelper.AstNodeToValue(childNode.AstNode)).OfType<MemberValue>())
+                    // 2. set member values by MemberValues (so that we can overwrite the copied members if we want)
+                    foreach (var memberValue in childValues.OfType<MemberValue>())
                     {
                         if (memberValue.MemberInfo is PropertyInfo)
                             ((PropertyInfo)memberValue.MemberInfo).SetValue(objValue, memberValue.Value);
@@ -55,6 +54,40 @@ namespace Irony.ITG
 
                 base.Rule = value;
             }
+        }
+
+        protected static void MemberwiseCopyExceptNullValues(object destination, object source)
+        {
+            MemberwiseCopy(destination, source, fieldValue => fieldValue != null);
+        }
+
+        protected static void MemberwiseCopy(object destination, object source)
+        {
+            MemberwiseCopy(destination, source, fieldValue => true);
+        }
+
+        protected static void MemberwiseCopy(object destination, object source, Predicate<object> cloneFieldValue)
+        {
+            Type destinationType = destination.GetType();
+            Type sourceType = source.GetType();
+
+            if (!destinationType.IsAssignableFrom(sourceType))
+                throw new ArgumentException(string.Format("{0} is not assignable from {1}", destinationType.Name, sourceType.Name), "source");
+
+            foreach (FieldInfo fieldInfo in GetAllFields(sourceType, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                object fieldValue = fieldInfo.GetValue(source);
+                if (cloneFieldValue(fieldValue))
+                    fieldInfo.SetValue(destination, fieldValue);
+            }
+        }
+
+        protected static IEnumerable<FieldInfo> GetAllFields(Type type, BindingFlags bindingFlags)
+        {
+            return type.GetFields(bindingFlags)
+                .Concat(type.BaseType != null
+                    ? GetAllFields(type.BaseType, bindingFlags)
+                    : new FieldInfo[0]);
         }
 
         public BnfExpression RuleTL { get { return base.Rule; } set { base.Rule = value; } }
