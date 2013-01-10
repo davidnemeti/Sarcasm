d5d0d224217ea3e94bab3b009184005ba3d3387d
cooldavee <cooldavee@0e92fcd8-05cc-4690-8748-b30b12f507e5>
2013-01-10 06:08:18 +0000

    git-svn-id: http://Beast/svn/ETUS/trunk/ETUS@477 0e92fcd8-05cc-4690-8748-b30b12f507e5

---
+++ Irony.ITG/Misc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+using Irony;
+using Irony.Ast;
+using Irony.Parsing;
+using Irony.ITG;
+using Irony.ITG.Ast;
+using Irony.ITG.Unparsing;
+
+namespace Irony.ITG
+{
+    public static class Misc
+    {
+        public static void SetFormatting(this Parser parser, Formatting formatting)
+        {
+            parser.Context.Culture = (CultureInfo)formatting.FormatProvider;    // note: this will throw an InvalidCastException if the value is not a CultureInfo
+        }
+    }
+}
