c3af7ed1a06387d2e1e9a34e7cf824f800c886c9
Dávid Németi <david.nemeti@gmail.com>
2013-01-23 13:32:39 +0100

    fix Sarcasm solution

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -14,33 +14,30 @@ using Sarcasm;
 using Sarcasm.Ast;
 using Sarcasm.Unparsing;
 
-using ETUS.DomainModel;
-using ETUS.Grammar;
-
 namespace ConsoleApplication1
 {
     class Program
     {
-        static string path = @"C:\Users\dave\Documents\Programming\ETUS\ETUS.Predefined\Length.units";
+//        static string path = @"";
 
         static void Main(string[] args)
         {
-            var grammar = new UDLGrammar();
-            Console.WriteLine(grammar.GetNonTerminalsAsText());
-            //Console.WriteLine();
-            //Console.WriteLine(grammar.GetNonTerminalsAsText(omitBoundMembers: true));
+            //var grammar = new Grammar();
+            //Console.WriteLine(grammar.GetNonTerminalsAsText());
+            ////Console.WriteLine();
+            ////Console.WriteLine(grammar.GetNonTerminalsAsText(omitBoundMembers: true));
 
-            var parser = new Parser(grammar);
-            ParseTree parseTree = parser.Parse(File.ReadAllText(path), path);
+            //var parser = new Parser(grammar);
+            //ParseTree parseTree = parser.Parse(File.ReadAllText(path), path);
 
-            Unparser unparser = new Unparser(grammar);
+            //Unparser unparser = new Unparser(grammar);
 
-            Directory.CreateDirectory("unparse_logs");
-            var stream = File.Create(@"unparse_logs\unparsed_text");
-            unparser.Unparse(parseTree.Root.AstNode).WriteToStream(stream, unparser);
+            //Directory.CreateDirectory("unparse_logs");
+            //var stream = File.Create(@"unparse_logs\unparsed_text");
+            //unparser.Unparse(parseTree.Root.AstNode).WriteToStream(stream, unparser);
 
-            //string str = unparser.Unparse(parseTree.Root.AstNode).AsString(unparser);
-            //Console.WriteLine(str);
+            ////string str = unparser.Unparse(parseTree.Root.AstNode).AsString(unparser);
+            ////Console.WriteLine(str);
         }
     }
 }
