7b45ec8c816e523939bb58412f8f8bb00e26861e
Dávid Németi <david.nemeti@gmail.com>
2013-07-12 12:50:11 +0200

    .

--- MiniPL/Grammars/GrammarP.cs
+++ MiniPL/Grammars/GrammarP.cs
@@ -437,7 +437,7 @@ namespace MiniPL.Grammars
                     }
                     else if (unparsableObject.BnfTerm == B.PROGRAM)
                     {
-                        return new Decoration().Add(DecorationKey.FontStyle, FontStyles.Italic);
+                        return new Decoration().Add(DecorationKey.FontStyle, FontStyles.Italic).Add(DecorationKey.Foreground, Colors.Purple);
                     }
                     else if (unparsableObject.BnfTerm == B.NumberLiteral && unparsableObject.Obj is int)
                     {
