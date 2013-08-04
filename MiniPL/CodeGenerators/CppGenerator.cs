c0ac985b516a07b9357aa17f3f195725c2aecb45
Dávid Németi <david.nemeti@gmail.com>
2013-08-04 15:36:54 +0200

    .

--- MiniPL/CodeGenerators/CppGenerator.cs
+++ MiniPL/CodeGenerators/CppGenerator.cs
@@ -24,8 +24,13 @@ namespace MiniPL.CodeGenerators
             return Generate((Program)root);
         }
 
-        private readonly AsyncLock _lock = new AsyncLock();
-        public AsyncLock Lock { get { return _lock; } }
+        /*
+         * NOTE: this generator class does not have any instance fields, therefore instances of this class don't have any state
+         * (the Generate method creates a CppGeneratorTemplate every time it's being called), thus we don't have to really block
+         * concurrent calls of Generate method. So we return a new instance of AsyncLock on every access of the Lock property,
+         * thus those who access this property and lock on it will find the lock always open, so they won't block.
+         * */
+        public AsyncLock Lock { get { return new AsyncLock(); } }
     }
 
     public partial class CppGeneratorTemplate
