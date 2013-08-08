0cfbd753b63bb6e1338af6fd2ecddc1f5285d4be
Dávid Németi <david.nemeti@gmail.com>
2013-08-08 14:44:16 +0200

    - deploy.ps has been included into the project - using Irony.PCL

---
+++ Deploy.ps1
@@ -0,0 +1,45 @@
+﻿param([string]$project)
+
+function CreateZipFile( [string] $zipfilename, [string] $sourcedir )
+{
+    [Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
+    $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+    [System.IO.Compression.ZipFile]::CreateFromDirectory( $sourcedir, $zipfilename, $compressionLevel, $false )
+}
+
+if ($project -eq "")
+{
+    $projects = [Management.Automation.Host.ChoiceDescription[]] (
+        (new-Object Management.Automation.Host.ChoiceDescription "&Irony", "Irony"),
+        (new-Object Management.Automation.Host.ChoiceDescription "&Sarcasm", "Sarcasm")
+        );
+
+    $projectIndex = $host.ui.PromptForChoice("Deploy", "Select project to deploy:", $projects, -1)
+
+    $project = $projects[$projectIndex].HelpMessage
+}
+
+$msbuildpath = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+
+if ($project -eq "Irony")
+{
+    & $msbuildpath ..\Irony\Irony\Irony.PCL.csproj /p:Configuration=Release
+    Copy-Item -Path ..\Irony\Irony\bin\Release\Irony.dll -Destination Libraries -Force
+}
+elseif ($project -eq "Sarcasm")
+{
+    & $msbuildpath Sarcasm.sln /p:Configuration=Release
+
+    New-Item -Path Downloads\Temp -ItemType directory -Force
+
+    Copy-Item -Path Sarcasm\bin\Release\Irony.dll -Destination Downloads\Temp -Force
+    Copy-Item -Path Sarcasm\bin\Release\Sarcasm.dll -Destination Downloads\Temp -Force
+    Copy-Item -Path DecorationConnector.WPF\bin\Release\DecorationConnector.WPF.dll -Destination Downloads\Temp -Force
+    Copy-Item -Path DecorationConnector.Forms\bin\Release\DecorationConnector.Forms.dll -Destination Downloads\Temp -Force
+
+    $zipFileName = "Downloads\Sarcasm" + "_" + (Get-Date).ToShortDateString().TrimEnd('.').Replace('.', '-') + ".zip"
+    Remove-Item -Path $zipFileName -Force
+    CreateZipFile -zipfilename $zipFileName -sourcedir Downloads\Temp
+
+    Remove-Item -Path Downloads\Temp -Force -Recurse
+}
