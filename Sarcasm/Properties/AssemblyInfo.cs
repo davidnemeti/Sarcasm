ef153c41a0ffb11ba9cc0bb1edcdefb11df8f586
Dávid Németi <david.nemeti@gmail.com>
2013-08-15 15:17:28 +0200

    AutoIncrementAssemblyFileVersion target directory change

--- Sarcasm/Properties/AssemblyInfo.cs
+++ Sarcasm/Properties/AssemblyInfo.cs
@@ -56,4 +56,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.18.0")]
+[assembly: AssemblyFileVersion("3.1.22.0")]
