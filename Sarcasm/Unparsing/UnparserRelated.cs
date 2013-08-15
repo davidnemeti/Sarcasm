8e9582a78272088c0876c2209b0e3ec7a11ee987
Dávid Németi <david.nemeti@gmail.com>
2013-08-15 07:33:27 +0200

    refactoring

--- Sarcasm/Unparsing/UnparserRelated.cs
+++ Sarcasm/Unparsing/UnparserRelated.cs
@@ -18,6 +18,28 @@ using System.Threading;
 
 namespace Sarcasm.Unparsing
 {
+    public delegate IEnumerable<UtokenValue> ValueUtokenizer<in T>(IFormatProvider formatProvider, UnparsableAst reference, T astValue);
+
+    public interface IUnparsableNonTerminal : INonTerminal
+    {
+        bool TryGetUtokensDirectly(IUnparser unparser, UnparsableAst self, out IEnumerable<UtokenValue> utokens);
+        IEnumerable<UnparsableAst> GetChildren(Unparser.ChildBnfTerms childBnfTerms, object astValue, Unparser.Direction direction);
+        int? GetChildrenPriority(IUnparser unparser, object astValue, Unparser.Children children, Unparser.Direction direction);
+    }
+
+    public interface IUnparser
+    {
+        int? GetPriority(UnparsableAst unparsableAst);
+        IFormatProvider FormatProvider { get; }
+    }
+
+    internal interface IPostProcessHelper
+    {
+        Formatter Formatter { get; }
+        Unparser.Direction Direction { get; }
+        Action<UnparsableAst> UnlinkChildFromChildPrevSiblingIfNotFullUnparse { get; }
+    }
+
     public static class UnparserExtensions
     {
         public static string AsText(this IEnumerable<Utoken> utokens, Unparser unparser)
@@ -124,27 +146,5 @@ namespace Sarcasm.Unparsing
         {
             return Formatter.PostProcess(utokens, postProcessHelper);
         }
-    }
-
-    public delegate IEnumerable<UtokenValue> ValueUtokenizer<in T>(IFormatProvider formatProvider, UnparsableAst reference, T astValue);
-
-    public interface IUnparsableNonTerminal : INonTerminal
-    {
-        bool TryGetUtokensDirectly(IUnparser unparser, UnparsableAst self, out IEnumerable<UtokenValue> utokens);
-        IEnumerable<UnparsableAst> GetChildren(Unparser.ChildBnfTerms childBnfTerms, object astValue, Unparser.Direction direction);
-        int? GetChildrenPriority(IUnparser unparser, object astValue, Unparser.Children children, Unparser.Direction direction);
-    }
-
-    public interface IUnparser
-    {
-        int? GetPriority(UnparsableAst unparsableAst);
-        IFormatProvider FormatProvider { get; }
-    }
-
-    internal interface IPostProcessHelper
-    {
-        Formatter Formatter { get; }
-        Unparser.Direction Direction { get; }
-        Action<UnparsableAst> UnlinkChildFromChildPrevSiblingIfNotFullUnparse { get; }
     }
 }
